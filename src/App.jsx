ed4d7f994ec636415cc2403cdc5c5f24d32ab7be
bkuddin <mdbkuddin@gmail.com>
2023-09-13 22:45:42 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <h1 className='text-lg bg-slate-800 text-white'>Knowledge Cafe</h1>
+     
+    </>
+  )
+}
+
+export default App

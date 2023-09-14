d5a5efabe589f28ac7e820d0db1a29c39a62f09e
bkuddin <mdbkuddin@gmail.com>
2023-09-14 06:41:32 +0600

    Designed Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
-
+import profile_pic from '../../assets/images/profile.png'
 
 const Header = () => {
     return (
-        <div>
-             <h1 className='text-lg bg-slate-800 text-white'>Knowledge Cafe</h1>
+        <div className='flex justify-between items-center p-4 mx-40 border-b-2'>
+             <h1 className='text-lg  text-gray-900 font-black'>Knowledge Cafe</h1>
+             <img src={profile_pic} alt="" />
         </div>
     );
 };

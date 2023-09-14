e2fcc2a35938df6d9088e9d44475399bd45c86d8
bkuddin <mdbkuddin@gmail.com>
2023-09-14 06:57:20 +0600

    Mapping all blog and made props and validate it

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 
 const Blogs = () => {
@@ -14,6 +15,10 @@ const Blogs = () => {
     return (
         <div className="md:w-2/3">
             <h3>Data: {blogs.length}</h3>
+
+            {
+                blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+            }
             
         </div>
     );
